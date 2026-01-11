a9ff98f0c66e912f30db222a7a5dce6adb781727
Aguri Cano <aguricano@gmail.com>
2026-01-11 00:45:06 -0500

    1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
@@ -13,6 +14,10 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 /* ***********************
